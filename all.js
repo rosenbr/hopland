b32be4905f82e25211e1c7af391eb690b98176ad
Richard Rosenberg <rosenbr73@gmail.com>
2022-01-26 13:36:25 -0800

    Vineyard state set up with key value pairs

---
+++ all.js
@@ -0,0 +1,116 @@
+const state = {
+    vineyard:[
+        {
+            name: 'Nelson Family',
+            hours: 'M-F, 9-5',
+            phone: '(510)555-5555',
+            blurb: 'Loreum Ipsum',
+            reservations: 'Y/N?',
+            directions: 'tee hee'
+        },
+        {
+            name: 'Jaxon Keys',
+            hours: 'M-F, 9-5',
+            phone: '(510)555-5555',
+            blurb: 'Loreum Ipsum',
+            reservations: 'Y/N?',
+            directions: 'tee hee'
+        },
+        {
+            name: 'Saracina',
+            hours: 'M-F, 9-5',
+            phone: '(510)555-5555',
+            blurb: 'Loreum Ipsum',
+            reservations: 'Y/N?',
+            directions: 'tee hee'
+        },
+        {
+            name: 'Golden Pig',
+            hours: 'M-F, 9-5',
+            phone: '(510)555-5555',
+            blurb: 'Loreum Ipsum',
+            reservations: 'Y/N?',
+            directions: 'tee hee'
+        },
+        {
+            name: 'Golden Pig Wine Shop',
+            hours: 'M-F, 9-5',
+            phone: '(510)555-5555',
+            blurb: 'Loreum Ipsum',
+            reservations: 'Y/N?',
+            directions: 'tee hee'
+        },
+        {
+            name: 'McNab Ridge',
+            hours: 'M-F, 9-5',
+            phone: '(510)555-5555',
+            blurb: 'Loreum Ipsum',
+            reservations: 'Y/N?',
+            directions: 'tee hee'
+        },
+        {
+            name: 'Brutocao',
+            hours: 'M-F, 9-5',
+            phone: '(510)555-5555',
+            blurb: 'Loreum Ipsum',
+            reservations: 'Y/N?',
+            directions: 'tee hee'
+        },
+        {
+            name: 'Cesar Toxqui',
+            hours: 'M-F, 9-5',
+            phone: '(510)555-5555',
+            blurb: 'Loreum Ipsum',
+            reservations: 'Y/N?',
+            directions: 'tee hee'
+        },
+        {
+            name: 'Terra Savia',
+            hours: 'M-F, 9-5',
+            phone: '(510)555-5555',
+            blurb: 'Loreum Ipsum',
+            reservations: 'Y/N?',
+            directions: 'tee hee'
+        },
+        {
+            name: 'Alta Orsa',
+            hours: 'M-F, 9-5',
+            phone: '(510)555-5555',
+            blurb: 'Loreum Ipsum',
+            reservations: 'Y/N?',
+            directions: 'tee hee'
+        },
+        {
+            name: 'Graziano',
+            hours: 'M-F, 9-5',
+            phone: '(510)555-5555',
+            blurb: 'Loreum Ipsum',
+            reservations: 'Y/N?',
+            directions: 'tee hee'
+        },
+        {
+            name: 'McFadden',
+            hours: 'M-F, 9-5',
+            phone: '(510)555-5555',
+            blurb: 'Loreum Ipsum',
+            reservations: 'Y/N?',
+            directions: 'tee hee'
+        },
+        {
+            name: 'Hopland Tap',
+            hours: 'M-F, 9-5',
+            phone: '(510)555-5555',
+            blurb: 'Loreum Ipsum',
+            reservations: 'Y/N?',
+            directions: 'tee hee'
+        },
+        {
+            name: 'Stockfarm & Campovida',
+            hours: 'M-F, 9-5',
+            phone: '(510)555-5555',
+            blurb: 'Loreum Ipsum',
+            reservations: 'Y/N?',
+            directions: 'tee hee'
+        }
+    ]
+}
